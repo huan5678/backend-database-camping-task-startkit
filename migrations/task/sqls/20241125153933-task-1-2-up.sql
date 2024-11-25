4e3f3add9bc7fd50402782efd67dba777f24484d
黃士桓 <huan5678@gmail.com>
2024-11-26 00:53:09 +0800

    GIT COMMIT MESSAGE:
    修復：修復用戶角色指派和付費積分追蹤
    
    - 更新用戶角色指派 SQL 查詢以正確引用
    - 更新 INSERT INTO 陳述式以包含購買積分和支付價格
    
    OR
    
    修復：修復用戶角色分配和付費積分追蹤
    
    - 更新用戶角色分配 SQL 查詢以正確引用
    - 更新 INSERT INTO 陳述式以包含購買積分和支付價格
    
    Signed-off-by: 黃士桓 <huan5678@gmail.com>

--- migrations/task/sqls/20241125153933-task-1-2-up.sql
+++ migrations/task/sqls/20241125153933-task-1-2-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-UPDATE "USER" SET "role" = "COACH" WHERE "email" IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io');
+UPDATE "USER" SET "role" = 'COACH' WHERE "email" IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io');
