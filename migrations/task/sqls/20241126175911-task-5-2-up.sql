36c822c680899512c307ac468fc3f41866d1df7d
黃士桓 <huan5678@gmail.com>
2024-11-27 03:24:07 +0800

    Here's the translation of the commit message into Traditional Chinese:
    修復：標記課程和預訂為取消
    
    - 新增課程預訂記錄
    -更新兩個數據庫記錄，分別標記課程和預訂為取消
    
    Note: I've followed the standard translation conventions for Traditional Chinese in the programming and tech industry, ensuring the formatting remains intact.
    Signed-off-by: 黃士桓 <huan5678@gmail.com>

--- migrations/task/sqls/20241126175911-task-5-2-up.sql
+++ migrations/task/sqls/20241126175911-task-5-2-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-UPDATE "COURSE_COOKING" SET "status" = '課程已取消', "canceled_at" = '2024-11-24 17:00:00' WHERE "user_id" = (SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io') AND "course_id" = (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE "email" = 'lee2000@hexschooltest.io'));
+UPDATE "COURSE_BOOKING" SET "status" = '課程已取消', "cancelled_at" = '2024-11-24 17:00:00' WHERE "user_id" = (SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io') AND "course_id" = (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE "email" = 'lee2000@hexschooltest.io'));
--- migrations/task/sqls/20241126180156-task-5-3-up.sql
+++ migrations/task/sqls/20241126180156-task-5-3-up.sql
@@ -1,4 +1,4 @@
 /* Replace with your SQL commands */
 INSERT INTO "COURSE_BOOKING" (user_id, course_id, status, booking_at)
 VALUES
-((SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'), (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE "email" = 'lee2000@hexschooltest.io')), '即將授課', '2024-11-24 17:10:25'),
+((SELECT id FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'), (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE "email" = 'lee2000@hexschooltest.io')), '即將授課', '2024-11-24 17:10:25');
